d023eddb91fef09455b4b64984faa53aeb46796e
VOX <jonasvox.2014@gmail.com>
2017-03-17 05:46:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
 import threading
+import time
 import peers
 import net
 import local as io
-isUser = False
 
-
-
+username = "annonymous"
 
 def startup():
-    if io.check(peers.peersFile) == False:
+    if io.check(peers.peersFile) == False: # checks for peers file
         print("error: missing peers file")
 
-try:
-   threading.Thread(target=net.checkForCommands).start()
-except:
-   print "Error: unable to start thread"
+    print(peers.getLocalPeers()) # prints peers
+    
+    threading.Thread(target=net.StartListen).start() # starts server
+    threading.Thread(target=routine).start() # starts routine code
 
+    usernm = input("\nusername (press enter for 'annonymous'): ")
+    if usernm != "":
+        global username
+        username = usernm
+    print("Connecting...")
+    time.sleep(5)
+    print(str(username)+" connected. \n\n\n")
+    print("")
+    
+    while 1:
+        messaging() # starts messaging code
+
+def routine():
+    while 1:
+        peers.cleanup() # clean up code, does nothing
+        peers.gatherAlives() # organizes who is online
+        peers.updatePeers() # tries to get more peers, expands network
+        time.sleep(30) # does ^^ this every 5 minutes
+
+def messaging():
+    msg = input(": ")
+    data = str(username) + ": " + str(msg)
+    for addr in peers.alives:
+        net.SendData("msg|"+data+"|"+net.id(), addr)
+        
 
 startup()
-
-if isUser:
-    print(peers.getLocalPeers())
-
-
-
