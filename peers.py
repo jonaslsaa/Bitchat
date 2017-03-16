c65b5b13eb28ec34f942708274beadaedf4558e2
VOX <jonasvox.2014@gmail.com>
2017-03-17 05:46:21 +0800

    Update peers.py

--- peers.py
+++ peers.py
@@ -1,10 +1,23 @@
 import local as io
+import net
 peersFile = "peers.dat"
-peers = []
-def updatePeers():
-    global peersFile
-    global peers
-    p = io.load(peersFile)
-    #net.getPeers(p[0])
+alives = []
 def getLocalPeers():
     return io.load(peersFile)
+def addPeer(peer):
+    localPeers = getLocalPeers()
+    for local in localPeers:
+        if local != peer:
+            io.append(str(peer), peersFile)
+            break
+def gatherAlives():
+    peers = getLocalPeers()
+    global alives
+    alives = []
+
+def cleanup():
+    pass
+
+def updatePeers():
+    for addr in alives:
+        SendData("updatepeers", addr)
