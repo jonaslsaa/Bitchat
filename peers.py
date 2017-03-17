a85cbc2464402b372f4cd546f6de6131e2984e3d
VOX <jonasvox.2014@gmail.com>
2017-03-18 00:31:00 +0800

    moved peers code to net

--- peers.py
+++ peers.py
@@ -1,7 +1,6 @@
 import local as io
 import net
 peersFile = "peers.dat"
-alives = []
 def getLocalPeers():
     return io.load(peersFile)
 def addPeer(peer):
@@ -10,33 +9,3 @@ def addPeer(peer):
         if local != peer:
             io.append(str(peer), peersFile)
             break
-def gatherAlives():
-    peers = getLocalPeers()
-    global alives
-    alives = []
-    #print(peers[0])
-    for addr in peers:
-        #print("addr: "+addr)
-        net.SendData("ping", addr)
-
-def cleanup():
-    clean = []
-    wasCleaned = False
-    global alives
-    for i in alives:
-        if i not in clean:
-            clean.append(i)
-            wasCleaned == True
-    if wasCleaned == True:
-        alives = clean
-            
-
-def updatePeers():
-    global alives
-    for addr in alives:
-        net.SendData("updatepeers", addr)
-
-def aliveAppend(x):
-    alives.append(x)
-def getAlives():
-    return alives
