21edc7fc23006a0188248abaade07d62dce50eed
VOX <jonasvox.2014@gmail.com>
2017-03-17 23:53:52 +0800

    working networking
    also optimized

--- net.py
+++ net.py
@@ -10,16 +10,19 @@ history = []
 def StartListen():
     host = "127.0.0.1"
     port = 1900
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    
     while 1:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((host, port))
         data, addr = sock.recvfrom(1024)
-        gotData(data, addr) # < upgrade to threading
-    sock.close()
+        gData = data.decode()
+        gAddr = addr[0]
+        threading.Thread(target=gotData, args=(gData,gAddr)).start()
+        #gotData(data.decode(), addr[0]) # in threading ^
+        sock.close()
     
 def gotData(rawdata, addr):
-    print(rawdata)
-    rawdata = data.split("|")
+    print("recv: "+rawdata)
+    data = rawdata.split("|")
     
     if data[0] == "msg":
         canPost = True
@@ -30,28 +33,31 @@ def gotData(rawdata, addr):
             if canPost == True:
                 print(data[1])
                 history.append(data[2])
-                for a in peers.alives:
+                alives = ps.getAlives()
+                for a in alives:
                     if a != addr:
                         SendData(rawdata, a)
     
     if data[0] == "ping":
         SendData("alive", addr)
-        peers.alives.append(addr)
+        ps.aliveAppend(addr)
     if data[0] == "alive":
-        peers.alives.append(addr)
+        ps.aliveAppend(addr)
     if data[0] == "peer":
-        if len(data[1]) >= 1:
-            peer.addPeer(data[1])
+        if len(data) >= 1:
+            ps.addPeer(data[1])
     if data[0] == "updatepeers":
-        if len(data[1]) >= 1:
-            peers = peers.getLocalPeers()
-            for p in peers:
+        if len(data) >= 1:
+            localpeers = ps.getLocalPeers()
+            for p in localpeers:
                 SendData("peer|"+str(p), addr)
 
                 
 def SendData(data, host):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-    sock.sendto(bytes(data, "utf-8"), (host, 1900))
+    print("sending: "+data+", to: "+host)
+    sock.sendto(bytes(data, "utf-8"), (host, 1902))
+    sock.close()
 
 
 def id(size=6, chars=string.ascii_lowercase + string.digits):
