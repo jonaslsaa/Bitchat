e7ccbf6e489adfe0a2b20aefe19186355f1a0962
TheVox <jonasvox.2014@gmail.com>
2017-07-16 13:38:22 +0200

    2

--- peers.py
+++ peers.py
@@ -2,9 +2,9 @@ import local as io
 import net
 peersFile = "peers.dat"
 def getLocalPeers():
-    print("added locally "+x)
     return io.load(peersFile)
 def addPeer(peer):
+    print("added locally: "+peer)
     localPeers = getLocalPeers()
     for local in localPeers:
         if local != peer:
